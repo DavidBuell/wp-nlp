d70ab97906d352a01c654134e4f67c3ef0f0dab4
Mark J Rawlins <mark.jeffrey.rawlins@gmail.com>
2022-04-05 12:47:05 +0900

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+const path = require("path")
+const webpack = require('webpack')
+
+module.exports = {
+    //we need a entry point with a relative path to the index.js file in the client folder
+    entry: "./client/index.js",
+}
