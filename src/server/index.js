0f84422ff609a922626f6db194929cc59d08755c
Mark J Rawlins <mark.jeffrey.rawlins@gmail.com>
2022-04-07 11:41:56 +0900

    rewriting API

--- src/server/index.js
+++ src/server/index.js
@@ -21,39 +21,18 @@ app.use(cors());
 
 // console.log("__dirname: ", __dirname);
 
+//this sends the index.html file to the client (hosts the static file)
 app.get('/*', (req, res) => {
     res.sendFile(path.resolve("dist", "index.html"));
 })
 
-//we need an empty array to store the data
+//we need an empty array to store the data in our server 'database'
 let data = [];
 
 //create a route that handles the post request for the new URL that comes from the form
-app.post('/api', async (req, res) => {
-    try {
-        //get the url from the request body
-        const url = req.body.url;
-        console.log(url);
-        //build the url to the api using the baseUrl and the apiKey
-        const apiUrl = `${baseUrl}${apiKey}&lang=en&mode=general&url=${url}`;
-        console.log(apiUrl);
-        //we need to fetch the data from the api
-        const data = await fetch(apiUrl);
-        console.log(data);
-        //we need to parse the data to json
-        const json = await data.json();
-        console.log(json);
-        //we need to send the data for confidence, irony, and agreement to the client
-        res.send({
-            confidence: json.confidence,
-            irony: json.irony,
-            agreement: json.agreement
-        });
-    } catch (error) {
-        console.log(error);
-    }
+app.post('/api', (req, res) => {
+    console.log("req.body: ", req.body);
 });
-
 
 app.listen(process.env.port || 3000, () => {
     console.log('Example app listening on port 3000!');
